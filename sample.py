8cfaf03264543ac9c3a92212ec567057db1ce8b5
Kasish Verma <137285698+kasishverma@users.noreply.github.com>
2023-10-03 21:50:07 +0530

    Add files via upload

--- sample.py
+++ sample.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 import os
 from streamlit_option_menu import option_menu
-import home, contact, projects, about, mailus
+#import home, contact, projects, about, mailus
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -59,77 +59,121 @@ selected=option_menu(
 )
 
 if selected=="home":
-    st.title("you selected home")
-if selected=="projects":
-    st.title("you selected projects")
-if selected=="contact":
-    st.title("you selected contact")
+    st.markdown(
+        "<p class='custom-text'><h2 style=text-align:center;>Dashboard</h2></p>",
+        unsafe_allow_html=True)
 
+
+
+
+    st.divider()
+    st.write("welcome to streamlit")
+
+
+    st.sidebar.header("Choose your filter: ")
+    slider_value=st.sidebar.slider("select a value",0,100,50)
+    checkbox_option=st.sidebar.checkbox("show data")
+    data=pd.DataFrame({'x':np.random.rand(100),'y':np.random.rand(100)})
+    if checkbox_option:
+        st.subheader("random data")
+        st.write(data)
+
+    #def load_image(image_path):
+            #return Image.open(image_path)
+    tab1, tab2 = st.tabs(["TAB1", "TAB2"])
+    with tab1:
+        col1, col2 = st.columns((2))
+        with col1:
+            #st.markdown("\n")
+            #st.markdown("\n")
+            st.markdown("<h5 style='text-align: center;'>H1</h5>",
+                        unsafe_allow_html=True)
+            images1_1 = load_image(
+                r"C:\Users\vermaks\Downloads\1.png")
+            images1_2 = load_image(
+                r"C:\Users\vermaks\Downloads\6.png")
+            st.image(images1_1, caption=" image3", use_column_width=True)
+            st.divider()
+            st.image(images1_2, caption=" image4", use_column_width=True)
+        with col2:
+            st.markdown("<h5 style='text-align: center;'>H2</h5>",
+                        unsafe_allow_html=True)
+            images2_1 = load_image(
+                r"C:\Users\vermaks\Downloads\3.png")
+            images2_2 = load_image(
+                r"C:\Users\vermaks\Downloads\4.png")
+            st.image(images2_1, caption=" image1", use_column_width=True)
+            st.divider()
+            st.image(images2_2, caption=" image2", use_column_width=True)
+    st.divider()
+    selected_option = st.selectbox("",['line graph', 'bar graph',
+                                                'histogram'])
+    # Define the paths to your images based on the selected option
+    if selected_option == "line graph":
+        image1_path = load_image(
+                    r"C:\Users\vermaks\Downloads\8.png")
+
+
+    elif selected_option == "bar graph":
+        image1_path = load_image(
+                    r"C:\Users\vermaks\Downloads\9.png")
+        
+    elif selected_option == "histogram":
+        image1_path = load_image(
+                    r"C:\Users\vermaks\Downloads\0.png")
+    st.image(image1_path, use_column_width=True)
+
+
+if selected=="projects":
+    with st.sidebar:
+    #st.sidebar.header("")
+        selected=option_menu(
+            menu_title=None,
+            options=["Analysis 1","Analysis 2","Analysis 3"], 
+        #icons=["house","book","envelope","phone"],
+            #default_index=0,
+            #orientation="vertical" ,
+        #styles={
+                    #"container": {"padding": "0!important", "background-color": "#f8f5fa"},
+                    # "icon": {"color": "orange", "font-size": "15px"},
+                    #"nav-link": {"font-size": "18px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
+                    #"menu-icon": {"color": "purple"},
+                    #"nav-link-selected": {"color": "black", "font-size": "12px", "font-weight": "bold",
+                    #                    "background-color": "#eee"},
+                    #"icon": {"color": "purple"},
+                    #"menu-title": {"font-size": "18px", "font-weight": "bold"}
+                #}
+    )
+
+    if selected=="Analysis 1":
+        st.write("Analysis1 report")
+    if selected=="Analysis 2":
+        st.write("Analysis2 report")
+    if selected=="Analysis 3":
+        st.write("Analysis3 report")
+
+if selected=="contact":
+    with st.sidebar:
+        selected=option_menu(
+            menu_title=None,
+            options=["department1"," 2"," 3"], 
+       
+    )
+
+    if selected=="department1":
+        st.write("..")
+    if selected==" 2":
+        st.write("...")
+    if selected==" 3":
+        st.write("....")
+
+
+
+if selected=="About":
+    
+    st.header("hi")
 
 
 st.markdown('<style>div.block-container{padding-top:0.05rem;}</style>',unsafe_allow_html=True)
 
 
-st.markdown(
-    "<p class='custom-text'><h2 style=text-align:center;>Dashboard</h2></p>",
-    unsafe_allow_html=True)
-
-
-
-
-st.divider()
-st.write("welcome to streamlit")
-
-
-st.sidebar.header("Choose your filter: ")
-slider_value=st.sidebar.slider("select a value",0,100,50)
-checkbox_option=st.sidebar.checkbox("show data")
-data=pd.DataFrame({'x':np.random.rand(100),'y':np.random.rand(100)})
-if checkbox_option:
-    st.subheader("random data")
-    st.write(data)
-
-#def load_image(image_path):
-        #return Image.open(image_path)
-tab1, tab2 = st.tabs(["TAB1", "TAB2"])
-with tab1:
-    col1, col2 = st.columns((2))
-    with col1:
-        #st.markdown("\n")
-        #st.markdown("\n")
-        st.markdown("<h5 style='text-align: center;'>H1</h5>",
-                    unsafe_allow_html=True)
-        images1_1 = load_image(
-            r"C:\Users\vermaks\Downloads\1.png")
-        images1_2 = load_image(
-            r"C:\Users\vermaks\Downloads\6.png")
-        st.image(images1_1, caption=" image3", use_column_width=True)
-        st.divider()
-        st.image(images1_2, caption=" image4", use_column_width=True)
-    with col2:
-        st.markdown("<h5 style='text-align: center;'>H2</h5>",
-                    unsafe_allow_html=True)
-        images2_1 = load_image(
-            r"C:\Users\vermaks\Downloads\3.png")
-        images2_2 = load_image(
-            r"C:\Users\vermaks\Downloads\4.png")
-        st.image(images2_1, caption=" image1", use_column_width=True)
-        st.divider()
-        st.image(images2_2, caption=" image2", use_column_width=True)
-st.divider()
-selected_option = st.selectbox("",['line graph', 'bar graph',
-                                            'histogram'])
-# Define the paths to your images based on the selected option
-if selected_option == "line graph":
-    image1_path = load_image(
-                r"C:\Users\vermaks\Downloads\8.png")
-
-
-elif selected_option == "bar graph":
-    image1_path = load_image(
-                r"C:\Users\vermaks\Downloads\9.png")
-    
-elif selected_option == "histogram":
-    image1_path = load_image(
-                r"C:\Users\vermaks\Downloads\0.png")
-st.image(image1_path, use_column_width=True)
