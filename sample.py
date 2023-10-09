5b537e53080adff57e3b8c9d837700c3fe469864
Kasish Verma <137285698+kasishverma@users.noreply.github.com>
2023-10-09 07:50:51 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,25 +1,21 @@
-# Define a CSS style for setting a background image for the entire page
-page_bg_img = '''
-<style>
-    # Set a linear gradient background from #cd9cf2 to #f6f3ff from top to bottom
-    [data-testid="stAppViewContainer"]{
-        background-image: linear-gradient(to top, #cd9cf2 0%, #f6f3ff 100%);
-    }
-
-    # Make the header background color transparent
-    [data-testid="stHeader"]{
-        background-color: rgba(0,0,0,0);
-    }
-
-    # Adjust the position of the toolbar to the right by 2rem
-    [data-testid="stToolbar"]{
-        right: 2rem;
-    }
-
-    # No specific styling for the sidebar is defined here
-    [data-testid="stSidebar"]{
-    }
-</style>
-'''
-
-# The above CSS code is intended to be applied to a Streamlit app's appearance
+with st.sidebar:
+        # Create a navigation menu with icons
+        selected = option_menu(
+            menu_title=None,
+            options=["home", "projects", "contact us", "About"],
+            icons=["house", "book", "envelope", "phone"],
+            # menu_icon = <i class ="graph-up-arrow" style="font-size: 2rem; color: cornflowerblue;" > < / i >
+            # menu_icon="graph-up-arrow",
+            # orientation="horizontal",
+            default_index=0,
+            styles={
+                "container": {"padding": "0!important", "background-color": "#f8f5fa"},
+                # "icon": {"color": "orange", "font-size": "15px"},
+                "nav-link": {"font-size": "14px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
+                "menu-icon": {"color": "grey"},
+                "nav-link-selected": {"color": "black", "font-size": "12px", "font-weight": "bold",
+                                      "background-color": "#eee"},
+                "icon": {"color": "purple"},
+                # "menu-title": {"font-size": "15px", "font-weight": "bold"}
+            }
+        )
