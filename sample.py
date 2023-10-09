ed3b859b8e9895410739a93a16050661e67dc1d3
Kasish Verma <137285698+kasishverma@users.noreply.github.com>
2023-10-09 12:17:05 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,20 +1,25 @@
+# Define a CSS style for setting a background image for the entire page
 page_bg_img = '''
 <style>
+    # Set a linear gradient background from #cd9cf2 to #f6f3ff from top to bottom
+    [data-testid="stAppViewContainer"]{
+        background-image: linear-gradient(to top, #cd9cf2 0%, #f6f3ff 100%);
+    }
 
-[data-testid="stAppViewContainer"]{
-    background-image: linear-gradient(to top, #cd9cf2 0%, #f6f3ff 100%);
-}
+    # Make the header background color transparent
+    [data-testid="stHeader"]{
+        background-color: rgba(0,0,0,0);
+    }
 
-[data-testid="stHeader"]{
-    background-color: rgba(0,0,0,0);
-}
+    # Adjust the position of the toolbar to the right by 2rem
+    [data-testid="stToolbar"]{
+        right: 2rem;
+    }
 
-[data-testid="stToolbar"]{
-    right: 2rem
-}
-
-[data-testid="stSidebar"]{
-}
-
+    # No specific styling for the sidebar is defined here
+    [data-testid="stSidebar"]{
+    }
 </style>
 '''
+
+# The above CSS code is intended to be applied to a Streamlit app's appearance
